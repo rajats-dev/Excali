cd4716fbd5fb60dfb2c50f70a6a802e8de3bec62
rajats-dev <rajatsundriyal1@gmail.com>
2026-02-17 10:32:05 +0530

    adding routes server & ws, packages layer

---
+++ apps/server/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/server-common/config";
+import { middleware } from "./middleware";
+import {
+  CreateUserSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+
+const app = express();
+
+app.post("./signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({ message: "Incorrect Input" });
+    return;
+  }
+  res.json({ userId: "123" });
+});
+
+app.post("/signin", (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({ message: "Incorrect Input" });
+    return;
+  }
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+
+  res.json({ token });
+});
+
+app.post("./room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({ message: "Incorrect Input" });
+    return;
+  }
+  res.json({ roomId: 1233 });
+});
+
+app.listen(3001);
