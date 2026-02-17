91d1807c62c13d6fa68980dc5bf635c58addcf1f
rajats-dev <rajatsundriyal1@gmail.com>
2026-02-17 20:45:17 +0530

    adding server layer

---
+++ apps/server/src/middleware.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/server-common/config";
+
+export function middleware(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers["authorization"] ?? "";
+
+  if (token) {
+    res.status(403).json({ message: "Unauthorized" });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      res.status(403).json({ message: "Unauthorized" });
+    }
+    // @ts-ignore
+    req.userId = user.userId;
+    next();
+  });
+}
